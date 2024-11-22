6a6c09279715f12fbd2d63008542ac79b3a89be4
itzjoaquinb <jbecerra@udesa.edu.ar>
2024-11-22 11:32:42 -0300

    category

--- js/category.js
+++ js/category.js
@@ -8,22 +8,22 @@ fetch(`https://dummyjson.com/recipes`)
         return response.json(); 
     })
     .then(function (data) {
-        let lista = document.querySelector(".contenedorRecetas")
+        let lista = document.querySelector("#contenedorRecetas")
         let titulo = document.querySelector("#tituloCategoria")
         titulo.innerHTML = `${categoryName}`
         let recetas = []
 
-        for (let i = 0; i < data.recipies.length; i++) {
-            let receta = recetas[i];
-            let recetaElemento = document.createElement('div');
-            recetaElemento.classList.add('receta');
-            recetaElemento.innerHTML = `
+        for (let i = 0; i < data.recipes.length; i++) {
+            if (data.recipes[i].tags.includes(categoryName)){
+                recetas += `
                 <img src="${receta.image}" alt="Imagen de ${receta.title}">
                 <h3>${receta.name}</h3>
                 <p>Dificultad: ${receta.difficulty}</p>
-                <a href="./receta.html?id=${receta.id}">Ver más</a>
-            `;
-            recetasContainer.appendChild(recetaElemento);
+                <a href="./receta.html?id=${receta.id}">Ver más</a>`
+
+            }
+            lista.innerHTML=recetas;
+
         }
     })
     .catch(function (error) {
