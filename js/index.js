a60d3c33162ed14778fde4885d5176ec7b2fee9d
itzjoaquinb <jbecerra@udesa.edu.ar>
2024-11-25 12:56:59 -0300

    changes index

--- js/index.js
+++ js/index.js
@@ -1,41 +1,34 @@
-const contenedorRecetas = document.getElementById('contenedorRecetas');
-const botonCargarMas = document.getElementById('botonCargarMas');
+let lista = document.querySelector("#contenedorRecetas");
+let botonCargarMas = document.querySelector("#botonCargarMas");
 let paginaActual = 0;
-const recetasPorPagina = 10;
+let recetasPorPagina = 10;
 
-function obtenerRecetas(pagina) {
-    return fetch(`https://dummyjson.com/recipes?limit=${recetasPorPagina}&skip=${pagina * recetasPorPagina}`)
-        .then(function (respuesta) {
-            return respuesta.json();
-        })
-        .then(function (datos) {
-            return datos.recipes;
-        })
-        .catch(function (error) {
-            console.error('Error al obtener recetas:', error);
-            return [];
-        });
-}
-
-function mostrarRecetas(recetas) {
-    recetas.forEach(function (receta) {
-        const tarjetaReceta = document.createElement('div');
-        tarjetaReceta.classList.add('recetaBox');
-        tarjetaReceta.innerHTML = `
-            <img src="${receta.image}" alt="${receta.title}" class="imagenReceta">
-            <h2 class="recipe-title">${receta.name}</h2>
-            <p class="recipe-difficulty">Dificultad: ${receta.difficulty}</p>
-            <a href="receta.html?id=${receta.id}" class="recipe-link">VER MÁS</a>
-        `;
-        contenedorRecetas.appendChild(tarjetaReceta);
-    });
-}
 
 function cargarRecetas() {
-    obtenerRecetas(paginaActual).then(function (recetas) {
-        mostrarRecetas(recetas);
-        paginaActual++;
-    });
+    fetch(`https://dummyjson.com/recipes?limit=${recetasPorPagina}&skip=${paginaActual * recetasPorPagina}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let recetas = '';
+
+            for (let i = 0; i < data.recipes.length; i++) {
+                recetas += `
+                <div class="receta">
+                    <img src="${data.recipes[i].image}" alt="Imagen de ${data.recipes[i].name}">
+                    <h3>${data.recipes[i].name}</h3>
+                    <p>Dificultad: ${data.recipes[i].difficulty}</p>
+                    <a href="./receta.html?id=${data.recipes[i].id}">Ver más</a>
+                </div>`;
+            }
+
+            lista.innerHTML += recetas;
+
+            paginaActual++;
+        })
+        .catch(function (error) {
+            console.error('Error al cargar las recetas:', error);
+        });
 }
 
 botonCargarMas.addEventListener('click', cargarRecetas);
